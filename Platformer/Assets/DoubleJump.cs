dc55ae3e52fdf1e07f44e6e3d5c6c7fb2d53a538
jrojasvalvo <jrojasva@andrew.cmu.edu>
2021-03-13 16:07:57 -0500

    double jump

---
+++ Platformer/Assets/DoubleJump.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJump : MonoBehaviour
+{
+    public float cooldown;
+    public float firstJumpHeight;
+    public float secondJumpHeight;
+    Transform t;
+    Rigidbody2D rb;
+    bool firstJump;
+    bool secondJump;
+    float jumpStartTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            if(firstJump) {
+                jumpStartTime = Time.time;
+                rb.velocity = (Vector2.up * firstJumpHeight);
+                firstJump = false;
+            } else if(secondJump && Time.time - jumpStartTime >= cooldown) {
+                rb.velocity = (Vector2.up * secondJumpHeight);
+                secondJump = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if(col.tag == "Platform") {
+            firstJump = true;
+            secondJump = true;
+        }
+    }
+}
