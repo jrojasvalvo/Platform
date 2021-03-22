f4e1e4e12987bcef2431d40f9fd771ba9d063b5f
jrojasvalvo <jrojasva@andrew.cmu.edu>
2021-03-22 00:29:39 -0400

    dialogue changes

--- Platformer/Assets/Scripts/Dialogue.cs
+++ Platformer/Assets/Scripts/Dialogue.cs
@@ -1,30 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
-    public TextMeshProUGUI textDisplay;
+    public TextMeshProUGUI textDisplay1;
+    public TextMeshProUGUI textDisplay2;
+    TextMeshProUGUI textDisplay;
     [TextArea(3, 10)]
     public string[] sentences;
     private int index;
     public float typingSpeed;
     public bool sentenceDone;
+    public bool sethActive;
+    public bool noahActive;
+    public Image Noah;
+    public Image Seth;
 
     void Start()
     {
-        StartCoroutine(Type());
+        textDisplay = textDisplay1;
+        StartCoroutine(Type(textDisplay));
         index = 0;
+        
+        sethActive = true;
+        noahActive = false;
     }
 
-    IEnumerator Type()
+    IEnumerator Type(TextMeshProUGUI t)
     {
         sentenceDone = false;
         foreach (char letter in sentences[index].ToCharArray())
         {
             if (sentenceDone) break;
-            textDisplay.text += letter;
+            t.text += letter;
             yield return new WaitForSeconds(typingSpeed); //default 0.02f
         }
         sentenceDone = true;
@@ -32,6 +43,15 @@ public class Dialogue : MonoBehaviour
 
     void Update()
     {
+        if (sethActive) {
+            Seth.gameObject.SetActive(true);
+            textDisplay = textDisplay1;
+            Noah.gameObject.SetActive(false);
+        } else if(noahActive) {
+            Noah.gameObject.SetActive(true);
+            textDisplay = textDisplay2;
+            Seth.gameObject.SetActive(false);
+        }
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))
         {
             Progress();
@@ -49,7 +69,7 @@ public class Dialogue : MonoBehaviour
         {
             index++;
             textDisplay.text = "";
-            StartCoroutine(Type());
+            StartCoroutine(Type(textDisplay));
         }
         else
         {
