0efd6c723b0acabf97ef12d60c31cfe50ef2be67
Shmeigo <jlessiohadi@gmail.com>
2021-03-28 18:01:28 -0400

    Pulling boxes

--- Platformer/Assets/Scripts/PushController.cs
+++ Platformer/Assets/Scripts/PushController.cs
@@ -6,6 +6,10 @@ public class PushController : MonoBehaviour
 {
     GameObject[] player;
     PlayerController p;
+    public bool pulling = false;
+    bool pullKeyDown = false;
+    Vector3 offset;
+    GameObject box;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +21,44 @@ public class PushController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (pulling) {
+            box.transform.position = p.transform.position - offset;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
+            pullKeyDown = true;
+        } if (Input.GetKeyUp(KeyCode.LeftControl)) {
+            pullKeyDown = false;
+        }
     }
 
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (pullKeyDown) {
+            pulling = true;
+            offset = p.transform.position - col.gameObject.transform.position;
+            box = col.gameObject;
+            p.firstJump = false;
+        } else if (!pullKeyDown) {
+            pulling = false;
+            p.firstJump = true;
+        }
+    }
     void OnTriggerStay2D(Collider2D col)
     {
         // only push if walking towards object
         if (col.tag == "Movable")
-        {
+        {   
+            if (pullKeyDown) {
+                pulling = true;
+                offset = p.transform.position - col.gameObject.transform.position;
+                box = col.gameObject;
+                p.firstJump = false;
+            } else if (!pullKeyDown) {
+                pulling = false;
+                p.firstJump = true;
+            }
+            
             if (p.movingLeft || p.movingRight)
             {
                 p.anim.SetBool("pushing", true);
